ab4f23083fd8bebb5b6b3e4cefa808f69921074a
princekk122 <awuahkarikari12@gmail.com>
2025-01-17 02:49:20 -0600

    feat: implement loading of colors and make the site interactive when get color button is clicked

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 const colorBtn = document.getElementById("color-btn");
 const modeType = document.getElementById("mode-type");
 const colorInput = document.getElementById("clr-type");
+const colorContainer = document.getElementById("color-wrapper");
 
 const BASE_URL = "https://www.thecolorapi.com/scheme";
-let color;
-let mode;
+let isLoading = false;
+
+const generateHTML = () => {
+  const results = [];
+  for (let i = 1; i <= 5; i++) {
+    results.push(`
+      <div class="container">
+        <div class="inner-container" id="container-${i}"></div>
+        <p class="color-code" id="code-${i}"></p>
+      </div>`);
+  }
+
+  return results.join("");
+};
+
+const render = (data) => {
+  const { colors } = data;
+  colorContainer.innerHTML = generateHTML();
+
+  for (let i = 0; i < colorContainer.children.length; i++) {
+    const bgClr = colors[i].hex.value;
+    document.getElementById(`container-${i + 1}`).style.backgroundColor = bgClr;
+    document.getElementById(`code-${i + 1}`).textContent = bgClr;
+  }
+};
 
 const fetchColorModes = (url) => {
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => render(data))
     .catch((err) => console.log(err));
 };
 
 colorBtn.addEventListener("click", function () {
   const color = colorInput.value.slice(1);
   const mode = modeType.value;
-  fetchColorModes(`${BASE_URL}?hex=${color}&mode=${mode}`);
+  isLoading = true;
+
+  if (isLoading) {
+    colorContainer.innerHTML = `
+      <div class="img-container">
+        <img class="loader" src="/images/loading.gif" alt="Loading gif"/>
+      </div>`;
+  }
+
+  setTimeout(function () {
+    isLoading = false;
+    fetchColorModes(`${BASE_URL}?hex=${color}&mode=${mode}`);
+  }, 1500);
 });
